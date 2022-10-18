3b43ce55f3dcabadf9b9912230223626f832ad71
NHSanto <nhsanto005@gmail.com>
2022-10-19 02:59:25 +0600

    Let's Start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button, Card} from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex align-items-center justify-content-center flex-column mt-2">
+      <Button variant='success'>Click Me</Button>
     </div>
   );
 }
