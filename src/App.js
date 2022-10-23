0a31d3fef6a11b13425f0ab782fc3bbce339d833
wpxmunna <wpxmunna@gmail.com>
2022-10-23 22:15:27 +0600

    sass added for style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import {Button, Card} from "react-bootstrap";
 
 function App() {
   return (
     <div className="d-flex align-items-center justify-content-center flex-column mt-2">
-      <Button variant='success'>Click Me</Button>
+        <Button variant='success'>Click Me</Button>
+        <Button variant="danger">Click Here</Button>
+        <h1>Hello</h1>
     </div>
   );
 }
